7a3533e29cfb4e9f0838abe9b1dd34a74cd7e901
Андрей Кузнецов <89307449447an@gmail.com>
2022-09-29 21:18:47 +0300

    Добавьте файлы проекта.

---
+++ SamRab/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SamRab
+{
+    class Class1
+    {
+        string path = @"C:\Users\murvi\Desktop\ReadWrite.txt";
+        public void Read ()
+        {
+            using (StreamReader reader = new StreamReader(path, Encoding.Default))
+            {
+                string read = reader.ReadToEnd();
+                Console.WriteLine("Содержимое документа");
+                Console.WriteLine(read);
+            }
+        }
+        public void Write()
+        {
+            using (FileStream file = new FileStream(path, FileMode.Append))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                {
+                    Console.WriteLine("Введите текст для добовления в документ");
+                    string write = Console.ReadLine();
+                    stream.WriteLine(write);
+                }
+            }
+        }
+    }
+}
---
+++ SamRab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SamRab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Read();
+            class1.Write();
+        }
+    }
+}
